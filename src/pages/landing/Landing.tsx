93de0aef973993bf33f6eabe983b6d9021730a41
godcl1623 <delphi5281@gmail.com>
2022-07-21 21:19:22 +0900

    #3 design : 이용약관 마크업 구현

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Terms from './components/terms/Terms';
 
 export default function Landing() {
-  return <h1>Landing</h1>;
+  // return <h1>Landing</h1>;
+  return <Terms contents="gathering" />;
 }
