5300f6f6252e02c9853ea7b646d6635a2e00a094
HyeonJu <hhjc017@gmail.com>
2022-07-22 19:49:07 +0900

    #8 refactor : 랜딩페이지 로고 분리

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Path from 'routes/Path';
+import Logo from './Logo';
 
 const button =
   'mb-4 p-4 hover:bg-neutral-500 ease-in duration-300 rounded-lg border-solid border-2 bg-buttonActive text-slate-50';
@@ -8,20 +9,7 @@ const button =
 export default function Landing() {
   return (
     <section className="flex flex-col justify-around w-full max-w-md h-screen mx-auto p-8 bg-slate-50 text-center">
-      <section>
-        <h1 className="flex justify-center items-center w-32 h-32 mx-auto mb-8 border-solid border-2 rounded-full border-neutral-600 text-2xl animate-[slideUp_1000ms_ease-out_forwards]">
-          SNP
-          <span className="block px-1 bg-neutral-600 text-slate-50 ">
-            LAB
-          </span>{' '}
-        </h1>
-        <p className="leading-5 animate-[slideUp_1500ms_ease-out_forwards]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, et.
-          Qui officia est aliquam placeat facere illum debitis veritatis
-          corporis voluptatem eligendi, eaque pariatur quo deleniti, quos omnis
-          vitae atque.
-        </p>
-      </section>
+      <Logo />
       <section className="flex flex-col animate-[slideUp_1500ms_ease-out_forwards]">
         <Link className={button} to={Path.Register}>
           지원하기
