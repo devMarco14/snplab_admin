d8bbe90f0c25e5bd72c91cadf8a8d2ac2d821cc5
godcl1623 <delphi5281@gmail.com>
2022-07-22 21:40:02 +0900

    #14 refactor : 이용약관 관련 요소, 로직들 TermsSection 컴포넌트로 분리

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,42 +1,20 @@
 import React from 'react';
-import { AiOutlineCheck } from 'react-icons/ai';
 import Modal from 'components/modal/Modal';
-import { StringIndexedObjects } from 'types/interfaces';
 import Terms from './components/terms/Terms';
-import TermsButton from './components/terms/TermsButton';
 import SelectRegion from './components/region/SelectRegion';
+import TermsSection from './components/terms/TermsSection';
 
 export default function Register() {
   const [isModalOpen, setModalOpen] = React.useState<boolean>(false);
-  const [areTermsChecked, setTermsChecked] = React.useState<
-    StringIndexedObjects<boolean>
-  >({
-    gathering: false,
-    thirdparty: false,
-  });
-  const allTermsChecked = Object.values(areTermsChecked).filter(
-    (status: boolean) => status,
-  ).length;
+  const [termsContents, setTermsContents] = React.useState('');
 
-  const termsContents = React.useRef<string>('');
-
-  const handleModal = React.useCallback(onClickHandleTerms, [isModalOpen]);
-
-  function onClickHandleTerms(event: React.MouseEvent) {
-    const currentTargetId = event.currentTarget.id;
+  const setModal = () => {
     setModalOpen(!isModalOpen);
-    termsContents.current = currentTargetId;
-  }
+  };
 
-  const handleCheck = React.useCallback(onClickHandleCheck, [areTermsChecked]);
-
-  function onClickHandleCheck(event: React.MouseEvent) {
-    const currentTargetId = event.currentTarget.id;
-    setTermsChecked({
-      ...areTermsChecked,
-      [currentTargetId]: !areTermsChecked[currentTargetId],
-    });
-  }
+  const setTerms = (value: string) => {
+    setTermsContents(value);
+  };
 
   const onCloseModal = React.useCallback(closeModal, [isModalOpen]);
 
@@ -58,43 +36,10 @@ export default function Register() {
           onFocus={() => setModalOpen(!isModalOpen)}
         />
       </section>
-      <section className="flex-center w-[300px] border-b-[2px] border-solid border-blackFont py-[10px]">
-        <button
-          type="button"
-          className={`flex-center w-[25px] h-[25px] rounded-full ${
-            allTermsChecked === 2 ? 'bg-blackFont' : 'bg-white'
-          }`}
-          onClick={() => {
-            setTermsChecked({
-              ...areTermsChecked,
-              gathering: allTermsChecked <= 1,
-              thirdparty: allTermsChecked <= 1,
-            });
-          }}
-        >
-          <AiOutlineCheck
-            className={`${
-              allTermsChecked === 2 ? 'text-white' : 'text-grayFont'
-            }`}
-          />
-        </button>
-        <p className="flex-1 ml-[5px]">이용약관 모두 동의</p>
-      </section>
-      <TermsButton
-        id="gathering"
-        handleModal={handleModal}
-        totalCheck={areTermsChecked}
-        handleCheck={handleCheck}
-      />
-      <TermsButton
-        id="thirdparty"
-        handleModal={handleModal}
-        totalCheck={areTermsChecked}
-        handleCheck={handleCheck}
-      />
+      <TermsSection setModal={setModal} setTerms={setTerms} />
       {isModalOpen && (
         <Modal onClick={onCloseModal}>
-          <Terms contents={termsContents.current} onClose={onCloseModal} />
+          <Terms contents={termsContents} onClose={onCloseModal} />
         </Modal>
       )}
       {isModalOpen && (
---
+++ src/pages/register/components/terms/TermsSection.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { AiOutlineCheck } from 'react-icons/ai';
+import { StringIndexedObjects } from 'types/interfaces';
+import TermsButton from './TermsButton';
+
+interface TermsSectionProps {
+  setModal: () => void;
+  setTerms: (value: string) => void;
+}
+
+export default function TermsSection({
+  setModal,
+  setTerms,
+}: TermsSectionProps) {
+  const [areTermsChecked, setTermsChecked] = React.useState<
+    StringIndexedObjects<boolean>
+  >({
+    gathering: false,
+    thirdparty: false,
+  });
+
+  const allTermsChecked = Object.values(areTermsChecked).filter(
+    (status: boolean) => status,
+  ).length;
+
+  const handleModal = React.useCallback(onClickHandleTerms, [
+    setModal,
+    setTerms,
+  ]);
+
+  function onClickHandleTerms(event: React.MouseEvent) {
+    const currentTargetId = event.currentTarget.id;
+    setModal();
+    setTerms(currentTargetId);
+  }
+
+  const handleCheck = React.useCallback(onClickHandleCheck, [areTermsChecked]);
+
+  function onClickHandleCheck(event: React.MouseEvent) {
+    const currentTargetId = event.currentTarget.id;
+    setTermsChecked({
+      ...areTermsChecked,
+      [currentTargetId]: !areTermsChecked[currentTargetId],
+    });
+  }
+
+  return (
+    <section>
+      <section className="flex-center w-[300px] border-b-[2px] border-solid border-blackFont py-[10px]">
+        <button
+          type="button"
+          className={`flex-center w-[25px] h-[25px] rounded-full ${
+            allTermsChecked === 2 ? 'bg-blackFont' : 'bg-white'
+          }`}
+          onClick={() => {
+            setTermsChecked({
+              ...areTermsChecked,
+              gathering: allTermsChecked <= 1,
+              thirdparty: allTermsChecked <= 1,
+            });
+          }}
+        >
+          <AiOutlineCheck
+            className={`${
+              allTermsChecked === 2 ? 'text-white' : 'text-grayFont'
+            }`}
+          />
+        </button>
+        <p className="flex-1 ml-[5px]">이용약관 모두 동의</p>
+      </section>
+      <TermsButton
+        id="gathering"
+        handleModal={handleModal}
+        totalCheck={areTermsChecked}
+        handleCheck={handleCheck}
+      />
+      <TermsButton
+        id="thirdparty"
+        handleModal={handleModal}
+        totalCheck={areTermsChecked}
+        handleCheck={handleCheck}
+      />
+    </section>
+  );
+}
