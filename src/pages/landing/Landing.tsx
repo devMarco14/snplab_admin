edb4f137771c6fb66866cc8eabe60036e6a71622
HyeonJu <hhjc017@gmail.com>
2022-07-24 14:37:19 +0900

    #8 design : 관리자 로그인 폼 모달

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import Modal from 'components/modal/Modal';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Path from 'routes/Path';
-import Logo from './Logo';
+import Logo from './components/Logo';
+import AdminLoginForm from './components/AdminLoginForm';
 
 const button =
-  'mb-4 p-4 hover:bg-neutral-500 ease-in duration-300 rounded-lg border-solid border-2 bg-buttonActive text-slate-50';
+  'mb-4 rounded-lg border-solid border-2 p-4 bg-buttonActive text-slate-50 hover:bg-neutral-500 ease-in duration-300 ';
 
 export default function Landing() {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const handleModalClick = () => {
+    setModalVisible((prevModalVisible) => !prevModalVisible);
+  };
+
   return (
-    <section className="flex flex-col justify-around w-full max-w-md h-screen mx-auto p-8 bg-slate-50 text-center">
+    <section className="relative flex flex-col justify-around w-full max-w-md h-screen mx-auto p-8 text-center">
       <Logo />
-      <section className="flex flex-col animate-[slideUp_1500ms_ease-out_forwards]">
+      <strong className="flex flex-col font-normal animate-[slideUp_1500ms_ease-out_forwards]">
         <Link className={button} to={Path.Register}>
           지원하기
         </Link>
-        <button className={button} type="submit">
+        <button className={button} type="submit" onClick={handleModalClick}>
           관리자 로그인
         </button>
-      </section>
+      </strong>
+      {isModalVisible && (
+        <>
+          <Modal onClick={handleModalClick} />
+          <AdminLoginForm onCancelClick={() => setModalVisible(false)} />
+        </>
+      )}
     </section>
   );
 }
---
+++ src/pages/landing/components/AdminLoginForm.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FiX as CancelIcon, FiUserCheck as UserIcon } from 'react-icons/fi';
+
+interface AdminLoginFormPropsType {
+  onCancelClick: (event: React.MouseEvent) => void;
+}
+
+const input = 'mb-2 p-2 rounded-lg border-solid border-2';
+const button =
+  'rounded-lg border-solid border-2 p-4 bg-blue-500 text-slate-50 hover:bg-blue-400 ease-in duration-300 ';
+
+function AdminLoginForm({ onCancelClick }: AdminLoginFormPropsType) {
+  const handleLogin = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form
+      className="modalChild flex flex-col justify-between h-1/2 min-h-[350px] w-8/12 min-w-[300px] z-10 rounded p-4 bg-slate-50"
+      onSubmit={handleLogin}
+    >
+      <strong className="font-normal">
+        <UserIcon className="mx-auto mb-4 text-6xl" />
+        <h1 className="text-base font-semibold">관리자 로그인</h1>
+      </strong>
+      <div className="flex flex-col">
+        <input type="email" placeholder="관리자 이메일" className={input} />
+        <input
+          type="password"
+          placeholder="관리자 비밀번호"
+          className={input}
+        />
+        <button type="submit" className={button}>
+          로그인
+        </button>
+        <button
+          className="absolute top-4 right-4 text-2xl hover:text-red-700 ease-in duration-300"
+          type="button"
+          onClick={onCancelClick}
+        >
+          <CancelIcon />
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default AdminLoginForm;
