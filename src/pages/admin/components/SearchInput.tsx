a601c95bc7bdce8d479d6d590c0fed7f9a63af26
chaengs <cathyshim222@naver.com>
2022-07-27 11:57:12 +0900

    #24 feature : 동적 placeholder 파일 분리

--- src/pages/admin/components/SearchInput.tsx
+++ src/pages/admin/components/SearchInput.tsx
@@ -3,33 +3,12 @@ import React, { useEffect, useState, useRef } from 'react';
 import { getWorkerInfo } from 'libs/api/admin';
 import { Members } from 'libs/types/members';
 import { HiOutlineSearch as SearchIcon } from 'react-icons/hi';
+import { dynamicPlaceholder } from 'libs/utils/dynamicPlaceholder';
 
 export default function SearchInput({ selectValue }: { selectValue: string }) {
   const [worker, setWorker] = useState<Members>();
   const [value, setValue] = useState('');
   const inputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
-
-  const changeText = (selectedValue: string) => {
-    let placeholderContent;
-    switch (selectedValue) {
-      case 'name':
-        placeholderContent = '지원자명을 검색하세요';
-        return placeholderContent;
-      case 'gender':
-        placeholderContent = "'여성' 또는 '남성'으로만 검색하세요";
-        return placeholderContent;
-      case 'birthday':
-        placeholderContent = '예시) 2022.01.01';
-        return placeholderContent;
-      case 'transportation':
-        placeholderContent =
-          '버스, 지하철, 택시, KTX/기차, 도보, 자전거, 전동킥보드, 자가용';
-        return placeholderContent;
-      case 'address':
-        placeholderContent = '시/도/군/구로 검색하세요';
-        return placeholderContent;
-    }
-  };
 
   useEffect(() => {
     if (selectValue) {
@@ -55,7 +34,7 @@ export default function SearchInput({ selectValue }: { selectValue: string }) {
         className="pl-2 w-[400px]"
         type="text"
         ref={inputRef}
-        placeholder={changeText(selectValue)}
+        placeholder={dynamicPlaceholder(selectValue)}
       />
       <button type="submit">
         <SearchIcon className="w-[20px] h-[20px] text-gray-400 mr-3" />
